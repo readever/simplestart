3cedefd4a96f702cd96b2019efccd9f0e8643fe2
readever <readever@163.com>
2024-09-19 13:35:59 +0800

    Add files via upload

--- demo/simplestart online/app.py
+++ demo/simplestart online/app.py
@@ -1,1 +1,30 @@
+import simplestart as ss
 
+ss.md("## SimpleStart Introduction")
+
+ss.space()
+ss.md('''
+### A Python Web Development Tool Without Frontend Programming
+SimpleStart is a tool that allows web development without the need for frontend coding. All development work is done in Python on the backend, enabling users to easily create web applications without needing to master HTML, CSS, JavaScript, Ajax, and other technologies.
+''')
+
+ss.space()
+
+ss.md('''
+### Offers a Rich Set of Components
+SimpleStart provides a variety of components needed for web development, including text, buttons, lists, tables, audio, and video. It supports containers and layouts while allowing users to customize components.
+''')
+
+ss.space()
+
+ss.md('''
+### Provides Data Visualization Support
+SimpleStart enables data visualization through images, tables, Plot charts, and SQLite databases. It supports both static and interactive charts and offers features for displaying and editing data.
+''')
+
+ss.space()
+ss.md("---")
+ss.write("For installation and usage information, please visit")
+ss.md('''
+[Online Help](http://www.simplestart.cc)
+''')
---
+++ demo/simplestart online/components/mycell.vue
@@ -0,0 +1,27 @@
+<template>
+    <!--<el-tag>{{item.value}}</el-tag>-->
+    <v-row>
+    <v-rating
+      v-model="rating" density="compact" color="orange" half-increments></v-rating>
+    <v-chip variant="text">{{item.value}}</v-chip>
+        </v-row>
+</template>
+
+<script>
+
+module.exports = {
+    props:["item"],
+    
+    data () {
+        return {
+            rating: 3
+        }
+    },
+    methods: {
+        //todo
+    },
+    mounted: function () {
+        this.rating = this.item.value/20
+    },
+}
+</script>
